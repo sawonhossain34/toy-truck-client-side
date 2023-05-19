f3e4db3af59b32a7d2eab9dbadd30953dfb4c23e
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-19 11:17:39 +0600

    login page create done

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,47 @@
+import login from '../../assets/images/login/login-img.jpg';
+
+const Login = () => {
+
+
+    const handleLogin = event => {
+        event.preventDefault();
+
+    }
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row ">
+                <div className=" w-1/2 mr-10">
+                    <img className='h-[400px]' src={login} alt="" />
+
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-5xl font-bold text-center">Login !</h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className='btn btn-secondary' type="submit" value="Login" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
