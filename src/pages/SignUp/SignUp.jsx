a8273bcdeff32ef621c4ca22d31a1bbbc20b6ecf
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-19 14:23:38 +0600

    authProvider create add

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,24 +1,30 @@
 import { Link } from 'react-router-dom';
 import login from '../../assets/images/login/login-img.jpg';
 
-const Login = () => {
 
-
-    const handleLogin = event => {
-        event.preventDefault();
-
+const SignUp = () => {
+    
+ 
+    const handleSignup = event => {
+        event.preventDefault()
     }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row ">
                 <div className=" w-1/2 mr-10">
-                    <img className='h-[400px]' src={login} alt="" />
+                    <img className='h-[500px]' src={login} alt="" />
 
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <h1 className="text-5xl font-bold text-center">Login !</h1>
-                        <form onSubmit={handleLogin}>
+                        <h1 className="text-5xl font-bold text-center">Sign Up !</h1>
+                        <form onSubmit={handleSignup}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -35,10 +41,10 @@ const Login = () => {
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <input className='btn btn-secondary' type="submit" value="Login" />
+                                <input className='btn btn-secondary' type="submit" value="sign up" />
                             </div>
                         </form>
-                        <p>Do not have an account? <Link className='text-pink-600 font-bold' to='/signup'>sign up</Link> </p>
+                        <p>Allready have an account? <Link className='text-pink-600 font-bold' to='/login'>Login</Link> </p>
                     </div>
                 </div>
             </div>
@@ -46,4 +52,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
