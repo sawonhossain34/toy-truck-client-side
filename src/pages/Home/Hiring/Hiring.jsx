3c4ffdce199430e33ce490fa68b25c156a2079e9
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-21 12:48:54 +0600

    extra seation add discount done

--- src/pages/Home/Hiring/Hiring.jsx
+++ src/pages/Home/Hiring/Hiring.jsx
@@ -1,31 +1,31 @@
-
+import hiring from '../../../assets/images/quality/hiring.jpg';
 
 const Hiring = () => {
     return (
-        <div className="bg-red-800 p-24">
-            {/* <div className="card card-side  shadow-xl bg-red-800">
-                <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">New album is released!</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Listen</button>
-                    </div>
-                </div>
-            </div> */}
-            <h2 className="text-5xl font-extrabold text-center text-white">First Time Truck Hiring</h2>
-            <div className="flex relative bg-red-900 p-4">
-                <div className="w-1/2 text-white absolute bottom-0 left-5">
-                    <p className="font-bold">Enjoy</p>
-                    <h2 className="text-5xl font-extrabold">1500</h2>
-                    <p className="font-bold">Discount</p>
-                </div>
-                <div className="w-1/2 absolute left-0 right-5-20">
-                    <span><h2 className="text-5xl font-bold ">Welcome</h2></span>
-                    <p>To get the offer you need to use the promo code: WELCOME</p>
+        <div className="card md:card-side bg-red-300 p-5 shadow-xl my-12">
+            <figure><img src={hiring} alt="Album" /></figure>
+            <div className="card-body text-red-600 pl-12">
+            <p className='text-2xl'>Enjoy</p>
+                <h2 className="card-title text-6xl">Discount !!</h2>
+                <p className='text-4xl'>1500 <span className='text-2xl'>%</span> </p>
+                <div className="card-actions justify-end">
+                    <button className="btn bg-red-700">Shop Now</button>
                 </div>
             </div>
         </div>
+        // <div className="hero min-h-screen  bg-red-200 p-4">
+        //     <div className="hero-content flex-col lg:flex-row  ">
+        //         <div className='w-1/2 text-center text-white '>
+        //             <p className="font-bold">Enjoy</p>
+        //             <h2 className="text-5xl font-extrabold">1500</h2>
+        //             <p className="font-bold">Discount</p>
+        //         </div>
+        //         <div className="w-1/2">
+        //             <img src={hiring} className="max-w-sm rounded-lg shadow-2xl" />
+
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
